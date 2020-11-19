1a2eb4a5968f7a5480a5b65db4d515beb4969ab2
Nikita Tulenev <n1tulenev@gmail.com>
2020-11-19 19:14:04 +0300

    Add files support

--- src/WebSocketMockServer/Configuration/MockTemplatesConfiguration.cs
+++ src/WebSocketMockServer/Configuration/MockTemplatesConfiguration.cs
@@ -7,13 +7,20 @@ namespace WebSocketMockServer.Configuration
 {
     public class MockTemplatesConfiguration
     {
+        public class Template
+        {
+            public string? File { get; set; }
+
+            public IEnumerable<ResponseTemplate>? Responses { get; set; }
+        }
+
         public class ResponseTemplate
         {
-            public string? Text { get; set; }
+            public string? File { get; set; }
 
             public int? Delay { get; set; }
         }
 
-        public Dictionary<string, IEnumerable<ResponseTemplate>>? Templates { get; set; }
+        public IEnumerable<Template>? Templates { get; set; }
     }
 }
--- src/WebSocketMockServer/Storage/MockTemplateStorage.cs
+++ src/WebSocketMockServer/Storage/MockTemplateStorage.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.Extensions.Options;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Options;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.IO;
 using System.Linq;
 using WebSocketMockServer.Configuration;
 using WebSocketMockServer.Templates;
@@ -9,15 +12,58 @@ namespace WebSocketMockServer.Storage
 {
     public class MockTemplateStorage : IMockTemplateStorage
     {
-        public MockTemplateStorage(IOptions<MockTemplatesConfiguration> config)
+        public MockTemplateStorage(IOptions<MockTemplatesConfiguration> config, IWebHostEnvironment _hostingEnvironment)
         {
-            var configData = config.Value!;
+
+            if (config == null)
+                throw new ArgumentNullException(nameof(config));
+
+            var configData = config.Value;
+
+            if (configData == null)
+                throw new ArgumentException("Value of the config is not set.", nameof(config));
 
             _templates = new Dictionary<string, MockTemplate>();
 
+            if (configData.Templates == null)
+                throw new ArgumentException("Templates of the config are not set.", nameof(config));
+
             foreach (var template in configData.Templates!)
             {
-                AddTemplate(new MockTemplate(template.Key, template.Value.Select(x => x.Delay is null ? new Response(x.Text!) : new Response(x.Text!, x.Delay.Value))));
+                if (string.IsNullOrWhiteSpace(template.File))
+                    throw new ArgumentException("Template file path not set.", nameof(config));
+
+                var keyFileName = Path.Combine(_hostingEnvironment.ContentRootPath, template.File);
+
+                var keyText = File.ReadAllText(keyFileName);
+
+                var responses = new List<Response>();
+
+                if (template.Responses == null)
+                    throw new ArgumentException($"Template {template.File} responses not set.", nameof(config));
+
+                foreach (var res in template.Responses)
+                {
+                    if (string.IsNullOrWhiteSpace(res.File))
+                        throw new ArgumentException($"Template {template.File} response file path not set.", nameof(config));
+
+                    var keyResFileName = Path.Combine(_hostingEnvironment.ContentRootPath, res.File);
+
+                    var resText = File.ReadAllText(keyResFileName);
+
+                    if (res.Delay.HasValue)
+                    {
+                        responses.Add(new Response(resText, res.Delay.Value));
+                    }
+                    else
+                    {
+                        responses.Add(new Response(resText));
+                    }
+                }
+
+                AddTemplate(new MockTemplate(keyText, responses));
+
+
             }
         }
 
@@ -40,6 +86,5 @@ namespace WebSocketMockServer.Storage
         }
 
         private readonly IDictionary<string, MockTemplate> _templates;
-
     }
 }
