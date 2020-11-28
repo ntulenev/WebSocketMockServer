8d3e2f9b52f4033b65105b36e465848e61a6bd19
Nikita Tulenev <n1tulenev@gmail.com>
2020-11-28 22:12:15 +0300

    Add MockTemplateStorage tests

--- src/WebSocketMockServer.Tests/MockTemplateStorage.Tests.cs
+++ src/WebSocketMockServer.Tests/MockTemplateStorage.Tests.cs
@@ -1,16 +1,71 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 using FluentAssertions;
 
 using Xunit;
 
+using WebSocketMockServer.Loader;
+using WebSocketMockServer.Storage;
+using Moq;
+using System.Threading;
+using System.Collections.Generic;
 using WebSocketMockServer.Models;
 
 namespace WebSocketMockServer.Tests
 {
     public class MockTemplateStorageTests
     {
+        [Fact(DisplayName = "MockTemplateStorage can not be created with null loader.")]
+        [Trait("Category", "Unit")]
+        public void CantCreateMockTemplateStorageWithNullLoader()
+        {
+            //Arrange
+            ILoader loader = null!;
+
+            // Act
+            var exception = Record.Exception(
+                () => new MockTemplateStorage(loader, null!));
+
+            // Assert
+            exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+        }
+
+        [Fact(DisplayName = "MockTemplateStorage creates empty storate on loader exception.")]
+        [Trait("Category", "Unit")]
+        public void MockTemplateCreatesEmptyStorageOnLoaderException()
+        {
+            //Arrange
+            var loader = new Mock<ILoader>();
+            loader.Setup(x => x.GetLoadedData()).Throws<InvalidOperationException>();
+
+            // Act
+            var exception = Record.Exception(
+                () => new MockTemplateStorage(loader.Object, null!));
+
+            // Assert
+            exception.Should().BeNull();
+        }
+
+        [Fact(DisplayName = "MockTemplateStorage creates storate for correct loader.")]
+        [Trait("Category", "Unit")]
+        public void MockTemplateCreatesStorageOnLoader()
+        {
+            //Arrange
+            var loader = new Mock<ILoader>();
+
+            var testDictionary = new Dictionary<string, MockTemplate>();
+            var template = new MockTemplate("aaa", new[] { new Response("bbb") });
+            testDictionary.Add(template.Request, template);
+
+            loader.Setup(x => x.GetLoadedData()).Returns(testDictionary);
+            var storage = new MockTemplateStorage(loader.Object, null!);
+
+            // Act
+            var status = storage.TryGetTemplate(template.Request, out var result);
+
+            // Assert
+            status.Should().BeTrue();
+            result.Should().Be(template);
+        }
     }
 }
