244e54dd09da0f341695fcba2d6355be6d856404
Nikita Tulenev <n1tulenev@gmail.com>
2020-11-28 17:01:08 +0300

    Add Response Tests

---
+++ src/WebSocketMockServer.Tests/Response.Tests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+using FluentAssertions;
+
+using Xunit;
+
+using WebSocketMockServer.Models;
+
+namespace WebSocketMockServer.Tests
+{
+    public class ResponseTests
+    {
+        [Fact(DisplayName = "Response can not be created with null message.")]
+        [Trait("Category", "Unit")]
+        public void CantCreatResponseWithNullMessage()
+        {
+            //Arrange
+            string msg = null!;
+
+            // Act
+            var exception = Record.Exception(
+                () => new Response(msg));
+
+            // Assert
+            exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+        }
+
+        [Theory(DisplayName = "Response can not be created with empty message.")]
+        [InlineData("")]
+        [InlineData("     ")]
+        [Trait("Category", "Unit")]
+        public void CantCreatResponseWithEmptyMessage(string msg)
+        {
+            // Act
+            var exception = Record.Exception(
+                () => new Response(msg));
+
+            // Assert
+            exception.Should().NotBeNull().And.BeOfType<ArgumentException>();
+        }
+
+        [Fact(DisplayName = "Response can be created with valid message.")]
+        [Trait("Category", "Unit")]
+        public void ResponseCanBeCreated()
+        {
+            //Arrange
+            string msg = "aaa";
+
+            // Act
+            var exception = Record.Exception(
+                () => new Response(msg));
+
+            // Assert
+            exception.Should().BeNull();
+        }
+    }
+}
