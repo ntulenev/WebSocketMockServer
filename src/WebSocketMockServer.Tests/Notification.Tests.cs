248309c53d67d2cb5de9120e2bcaaab6e7e435bc
Nikita Tulenev <n1tulenev@gmail.com>
2020-11-28 17:04:26 +0300

    Add Notification tests

---
+++ src/WebSocketMockServer.Tests/Notification.Tests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+using FluentAssertions;
+
+using Xunit;
+
+using WebSocketMockServer.Models;
+
+namespace WebSocketMockServer.Tests
+{
+    public class NotificationTests
+    {
+        [Fact(DisplayName = "Notification can not be created with null message.")]
+        [Trait("Category", "Unit")]
+        public void CantCreatResponseWithNullMessage()
+        {
+            //Arrange
+            string msg = null!;
+            int delay = 1;
+
+            // Act
+            var exception = Record.Exception(
+                () => new Notification(msg, delay));
+
+            // Assert
+            exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+        }
+
+        [Theory(DisplayName = "Notification can not be created with empty message.")]
+        [InlineData("")]
+        [InlineData("     ")]
+        [Trait("Category", "Unit")]
+        public void CantCreatResponseWithEmptyMessage(string msg)
+        {
+            //Arrange
+            int delay = 1;
+
+            // Act
+            var exception = Record.Exception(
+                () => new Notification(msg, delay));
+
+            // Assert
+            exception.Should().NotBeNull().And.BeOfType<ArgumentException>();
+        }
+
+        [Fact(DisplayName = "Notification can not be created with incorrect delay.")]
+        [Trait("Category", "Unit")]
+        public void CantCreatResponseWithBadDelay()
+        {
+            //Arrange
+            string msg = "aaa";
+            int delay = 0;
+
+            // Act
+            var exception = Record.Exception(
+                () => new Notification(msg, delay));
+
+            // Assert
+            exception.Should().NotBeNull().And.BeOfType<ArgumentException>();
+        }
+
+        [Fact(DisplayName = "Notification can be created with valid message.")]
+        [Trait("Category", "Unit")]
+        public void ResponseCanBeCreated()
+        {
+            //Arrange
+            string msg = "aaa";
+            int delay = 1;
+
+            // Act
+            var exception = Record.Exception(
+                () => new Notification(msg, delay));
+
+            // Assert
+            exception.Should().BeNull();
+        }
+    }
+}
