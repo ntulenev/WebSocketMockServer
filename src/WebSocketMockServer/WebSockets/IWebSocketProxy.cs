e75c8e0c252830d5f396bfaae9d7d6fdb9661f87
Nikita Tulenev <n1tulenev@gmail.com>
2020-12-12 11:32:51 +0300

    Add WebSocketProxy comments

--- src/WebSocketMockServer/WebSockets/IWebSocketProxy.cs
+++ src/WebSocketMockServer/WebSockets/IWebSocketProxy.cs
@@ -1,20 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.WebSockets;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace WebSocketMockServer.WebSockets
 {
+    /// <summary>
+    /// Wrapper that gives abstraction of <see cref="WebSocket"/> for other layers.
+    /// </summary>
     public interface IWebSocketProxy
     {
+        /// <summary>
+        /// Closes web socket
+        /// </summary>
+
         public Task CloseAsync(CancellationToken ct);
 
+        /// <summary>
+        /// Sends message to web socket
+        /// </summary>
         public Task SendMessageAsync(string msg, CancellationToken ct);
 
+        /// <summary>
+        /// Receives data from web socket.
+        /// </summary>
         public ValueTask<ValueWebSocketReceiveResult> ReceiveAsync(Memory<byte> buffer, CancellationToken cancellationToken);
 
+        /// <summary>
+        /// Web socket state.
+        /// </summary>
         public WebSocketState State { get; }
     }
 }
