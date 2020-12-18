b5514d7c8470748fc432fa5a7f591a4f51f49947
Nikita Tulenev <n1tulenev@gmail.com>
2020-12-18 20:04:30 +0300

    Add web socket proxy tests

--- src/WebSocketMockServer.Tests/WebSocketProxy.Tests.cs
+++ src/WebSocketMockServer.Tests/WebSocketProxy.Tests.cs
@@ -8,6 +8,8 @@ using Xunit;
 using Moq;
 
 using WebSocketMockServer.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace WebSocketMockServer.Tests
 {
@@ -58,6 +60,61 @@ namespace WebSocketMockServer.Tests
 
             // Assert
             wStatus.Should().Be(status);
+        }
+
+        [Fact(DisplayName = "WebSocket proxy runs ReceiveAsync properly.")]
+        [Trait("Category", "Unit")]
+        public async Task ReceiveAsyncShouldWorkProperly()
+        {
+            //Arrange
+            var ws = new Mock<WebSocket>();
+            var wresult = new ValueWebSocketReceiveResult(1, WebSocketMessageType.Binary, true);
+            var vtResult = new ValueTask<ValueWebSocketReceiveResult>(Task.FromResult(wresult));
+            ws.Setup(x => x.ReceiveAsync(It.IsAny<Memory<byte>>(), It.IsAny<CancellationToken>())).Returns(vtResult);
+            var proxy = WebSocketProxy.Create(ws.Object, null!);
+
+            // Act
+            var result = await proxy.ReceiveAsync(null, CancellationToken.None).ConfigureAwait(false);
+
+            // Assert
+            wresult.Should().Be(result);
+            ws.Verify(x => x.ReceiveAsync(It.IsAny<Memory<byte>>(), It.IsAny<CancellationToken>()), Times.Once);
+        }
+
+        [Fact(DisplayName = "WebSocket proxy runs SendMessageAsync properly.")]
+        [Trait("Category", "Unit")]
+        public void SendMessageAsyncProperly()
+        {
+            //Arrange
+            var ws = new Mock<WebSocket>();
+            ws.Setup(x => x.SendAsync(It.IsAny<ReadOnlyMemory<byte>>(), WebSocketMessageType.Text, true, It.IsAny<CancellationToken>())).Returns(new ValueTask(Task.CompletedTask));
+            ws.Setup(x => x.State).Returns(WebSocketState.Open);
+            var proxy = WebSocketProxy.Create(ws.Object, null!);
+
+            // Act
+            var t = proxy.SendMessageAsync("text", CancellationToken.None);
+
+            // Assert
+            t.IsCompleted.Should().BeTrue();
+            ws.Verify(x => x.SendAsync(It.IsAny<ReadOnlyMemory<byte>>(), WebSocketMessageType.Text, true, It.IsAny<CancellationToken>()), Times.Once);
+        }
+
+        [Fact(DisplayName = "WebSocket proxy runs SendMessageAsync properly for closed status.")]
+        [Trait("Category", "Unit")]
+        public void SendMessageAsyncSkipsProperly()
+        {
+            //Arrange
+            var ws = new Mock<WebSocket>();
+            ws.Setup(x => x.SendAsync(It.IsAny<ReadOnlyMemory<byte>>(), WebSocketMessageType.Text, true, It.IsAny<CancellationToken>())).Returns(new ValueTask(Task.CompletedTask));
+            ws.Setup(x => x.State).Returns(WebSocketState.Closed);
+            var proxy = WebSocketProxy.Create(ws.Object, null!);
+
+            // Act
+            var t = proxy.SendMessageAsync("text", CancellationToken.None);
+
+            // Assert
+            t.IsCompleted.Should().BeTrue();
+            ws.Verify(x => x.SendAsync(It.IsAny<ReadOnlyMemory<byte>>(), WebSocketMessageType.Text, true, It.IsAny<CancellationToken>()), Times.Never);
         }
 
         //ADD  ReceiveAsync SendMessageAsync CloseAsync with guard checks
