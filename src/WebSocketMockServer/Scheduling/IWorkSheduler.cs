322f85e73d4878cb1b34bdd07d71f8c386a450f5
Nikita Tulenev <n1tulenev@gmail.com>
2022-03-13 19:15:39 +0300

    Add sheduler xml doc

--- src/WebSocketMockServer/Scheduling/IWorkSheduler.cs
+++ src/WebSocketMockServer/Scheduling/IWorkSheduler.cs
@@ -1,7 +1,15 @@
 namespace WebSocketMockServer.Scheduling
 {
+    /// <summary>
+    /// Defines logic for scheduling some async logic to be done in future.
+    /// </summary>
     public interface IWorkSheduler
     {
+        /// <summary>
+        /// Schedules work.
+        /// </summary>
+        /// <param name="work">Work to be done.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
         public void Schedule(Func<Task> work, CancellationToken cancellationToken);
     }
 }
