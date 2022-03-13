bbd5b68201976eef0bd3a31fb7a8dfa423f9ba13
Nikita Tulenev <n1tulenev@gmail.com>
2022-03-13 19:15:57 +0300

    Add scheduler logs

--- src/WebSocketMockServer/Scheduling/WorkSheduler.cs
+++ src/WebSocketMockServer/Scheduling/WorkSheduler.cs
@@ -2,13 +2,42 @@ namespace WebSocketMockServer.Scheduling
 {
     public class WorkSheduler : IWorkSheduler
     {
+        /// <summary>
+        /// Creates <see cref="WorkSheduler"/>.
+        /// </summary>
+        /// <param name="logger">logger.</param>
+        /// <exception cref="ArgumentNullException">Throws is logger is null.</exception>
+        public WorkSheduler(ILogger<WorkSheduler> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        ///<inheritdoc/>
+        ///<exception cref="ArgumentNullException">If work is null.</exception>
+        ///<exception cref="OperationCanceledException">If token has cancellation.</exception>
         public void Schedule(Func<Task> work, CancellationToken cancellationToken)
         {
             ArgumentNullException.ThrowIfNull(work);
 
             cancellationToken.ThrowIfCancellationRequested();
 
-            _ = Task.Run(work, cancellationToken);
+            try
+            {
+                var t = Task.Run(work, cancellationToken);
+                _ = t.ContinueWith(t =>
+                {
+                    if (t.IsFaulted)
+                    {
+                        _logger.LogError(t.Exception, "Error in sheduled work");
+                    }
+                }, cancellationToken);
+            }
+            catch (Exception ex) when (ex is not OperationCanceledException)
+            {
+                _logger.LogError(ex, "Error on run sheduled work");
+            }
         }
+
+        private readonly ILogger<WorkSheduler> _logger;
     }
 }
