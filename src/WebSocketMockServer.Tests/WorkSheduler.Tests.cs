ad8a4c7b651b6ca31ae3351209aac7a7f9eb6499
Nikita Tulenev <n1tulenev@gmail.com>
2022-04-10 13:17:48 +0300

    Add WorkSheduler tests

--- src/WebSocketMockServer.Tests/WorkSheduler.Tests.cs
+++ src/WebSocketMockServer.Tests/WorkSheduler.Tests.cs
@@ -35,5 +35,83 @@ namespace WebSocketMockServer.Tests
             // Assert
             exception.Should().BeNull();
         }
+
+        [Fact(DisplayName = "WorkSheduler can't shedule null work.")]
+        [Trait("Category", "Unit")]
+        public void WorkShedulerCantSheduleNull()
+        {
+            // Arrange
+            using var cts = new CancellationTokenSource();
+            var scheduler = new WorkSheduler(Mock.Of<ILogger<WorkSheduler>>());
+
+            // Act
+            var exception = Record.Exception(
+                () => scheduler.Schedule(null!, cts.Token));
+
+            // Assert
+            exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+        }
+
+        [Fact(DisplayName = "WorkSheduler can't shedule if cancel.")]
+        [Trait("Category", "Unit")]
+        public void WorkShedulerCantSheduleIfCancel()
+        {
+            // Arrange
+            using var cts = new CancellationTokenSource();
+            cts.Cancel();
+            var scheduler = new WorkSheduler(Mock.Of<ILogger<WorkSheduler>>());
+
+            // Act
+            var exception = Record.Exception(
+                () => scheduler.Schedule(() => Task.CompletedTask, cts.Token));
+
+            // Assert
+            exception.Should().NotBeNull().And.BeOfType<OperationCanceledException>();
+        }
+
+        [Fact(DisplayName = "WorkSheduler can shedule work.")]
+        [Trait("Category", "Unit")]
+        public void WorkShedulerCanSheduleWork()
+        {
+            // Arrange
+            using var cts = new CancellationTokenSource();
+
+            var tcs = new TaskCompletionSource();
+
+            var scheduler = new WorkSheduler(Mock.Of<ILogger<WorkSheduler>>());
+
+            // Act
+            scheduler.Schedule(() =>
+            {
+                tcs.SetResult();
+                return Task.CompletedTask;
+            }, cts.Token);
+
+            // Assert
+            tcs.Task.Wait();
+        }
+
+        [Fact(DisplayName = "WorkSheduler can shedule work with error.")]
+        [Trait("Category", "Unit")]
+        public void WorkShedulerCanSheduleWorkWithError()
+        {
+            // Arrange
+            using var cts = new CancellationTokenSource();
+
+            var tcs = new TaskCompletionSource();
+
+            var scheduler = new WorkSheduler(Mock.Of<ILogger<WorkSheduler>>());
+
+            // Act
+            scheduler.Schedule(() =>
+            {
+                tcs.SetResult();
+                throw new InvalidOperationException();
+            }, cts.Token);
+
+            // Assert
+            tcs.Task.Wait();
+        }
+
     }
 }
