2077c84f25fb42283b161cf1d082744c4dc70816
Nikita Tulenev <n1tulenev@gmail.com>
2023-12-08 07:46:20 +0100

    Add XMLDoc for LoaderService

--- src/WebSocketMockServer/Services/LoaderService.cs
+++ src/WebSocketMockServer/Services/LoaderService.cs
@@ -3,20 +3,34 @@ using WebSocketMockServer.Loader;
 namespace WebSocketMockServer.Services;
 
 /// <summary>
-/// Hosted service that loads templates before web service starts.
+/// Represents a hosted service that loads templates before the web service starts.
 /// </summary>
 public class LoaderService : IHostedService
 {
+    private readonly ILogger<LoaderService> _logger;
+    private readonly IHostApplicationLifetime _hostApplicationLifetime;
+    private readonly ILoader _loader;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="LoaderService"/> class.
+    /// </summary>
+    /// <param name="logger">The logger to use for logging information.</param>
+    /// <param name="hostApplicationLifetime">The lifetime object for the hosted application.</param>
+    /// <param name="loader">The loader to use for loading templates.</param>
+    /// <exception cref="ArgumentNullException">Thrown when any argument is null.</exception>
     public LoaderService(ILogger<LoaderService> logger,
                          IHostApplicationLifetime hostApplicationLifetime,
-                         Loader.ILoader loader
-                                )
+                         ILoader loader)
     {
         _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         _hostApplicationLifetime = hostApplicationLifetime ?? throw new ArgumentNullException(nameof(hostApplicationLifetime));
         _loader = loader ?? throw new ArgumentNullException(nameof(loader));
     }
 
+    /// <summary>
+    /// Triggered when the application host is ready to start the service.
+    /// </summary>
+    /// <param name="cancellationToken">Indicates that the start process has been aborted.</param>
     public async Task StartAsync(CancellationToken cancellationToken)
     {
         try
@@ -30,9 +44,9 @@ public class LoaderService : IHostedService
         }
     }
 
+    /// <summary>
+    /// Triggered when the application host is performing a graceful shutdown.
+    /// </summary>
+    /// <param name="cancellationToken">Indicates that the shutdown process should no longer be graceful.</param>
     public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
-
-    private readonly ILogger<LoaderService> _logger;
-    private readonly IHostApplicationLifetime _hostApplicationLifetime;
-    private readonly ILoader _loader;
 }
