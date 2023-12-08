0c522f18b97f00ac83d27e8551baf5875dc6256d
Nikita Tulenev <n1tulenev@gmail.com>
2023-12-08 07:55:24 +0100

    Add primary constuctor for LoaderService

--- src/WebSocketMockServer/Services/LoaderService.cs
+++ src/WebSocketMockServer/Services/LoaderService.cs
@@ -5,28 +5,18 @@ namespace WebSocketMockServer.Services;
 /// <summary>
 /// Represents a hosted service that loads templates before the web service starts.
 /// </summary>
-public class LoaderService : IHostedService
+/// <remarks>
+/// Initializes a new instance of the <see cref="LoaderService"/> class.
+/// </remarks>
+/// <param name="logger">The logger to use for logging information.</param>
+/// <param name="hostApplicationLifetime">The lifetime object for the hosted application.</param>
+/// <param name="loader">The loader to use for loading templates.</param>
+/// <exception cref="ArgumentNullException">Thrown when any argument is null.</exception>
+public class LoaderService(
+                     ILogger<LoaderService> logger,
+                     IHostApplicationLifetime hostApplicationLifetime,
+                     ILoader loader) : IHostedService
 {
-    private readonly ILogger<LoaderService> _logger;
-    private readonly IHostApplicationLifetime _hostApplicationLifetime;
-    private readonly ILoader _loader;
-
-    /// <summary>
-    /// Initializes a new instance of the <see cref="LoaderService"/> class.
-    /// </summary>
-    /// <param name="logger">The logger to use for logging information.</param>
-    /// <param name="hostApplicationLifetime">The lifetime object for the hosted application.</param>
-    /// <param name="loader">The loader to use for loading templates.</param>
-    /// <exception cref="ArgumentNullException">Thrown when any argument is null.</exception>
-    public LoaderService(ILogger<LoaderService> logger,
-                         IHostApplicationLifetime hostApplicationLifetime,
-                         ILoader loader)
-    {
-        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
-        _hostApplicationLifetime = hostApplicationLifetime ?? throw new ArgumentNullException(nameof(hostApplicationLifetime));
-        _loader = loader ?? throw new ArgumentNullException(nameof(loader));
-    }
-
     /// <summary>
     /// Triggered when the application host is ready to start the service.
     /// </summary>
@@ -49,4 +39,12 @@ public class LoaderService : IHostedService
     /// </summary>
     /// <param name="cancellationToken">Indicates that the shutdown process should no longer be graceful.</param>
     public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+
+    private readonly ILogger<LoaderService> _logger = logger
+                                            ?? throw new ArgumentNullException(nameof(logger));
+    private readonly IHostApplicationLifetime _hostApplicationLifetime = hostApplicationLifetime
+                                            ?? throw new ArgumentNullException(nameof(hostApplicationLifetime));
+    private readonly ILoader _loader = loader
+                                            ?? throw new ArgumentNullException(nameof(loader));
+
 }
